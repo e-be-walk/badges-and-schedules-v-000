0c9b1b9e76a5afee100ec00a8adaaf316b40e5e8
Erin Walker <erinb0317@hotmail.com>
2018-01-28 23:35:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 
 def bage_maker(name)
-  puts "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}."
 end
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(attendees)
   attendees.each do |attendees|
-    puts "Hello, my name is #{attendees}"
+    puts "Hello, my name is #{attendees}."
   end
 end
