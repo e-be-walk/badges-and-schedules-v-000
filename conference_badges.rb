23fac62f6ec45dc4bbddbdd24eb27766cf9d070e
Erin Walker <erinb0317@hotmail.com>
2018-01-28 23:52:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,8 @@ end
 
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees)
   attendees.each do |badges|
    puts "Hello, my name is #{badges}."
   end
+end
