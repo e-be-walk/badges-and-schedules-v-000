1f47492b756b7e398242134ea3d82ca148714360
Erin Walker <erinb0317@hotmail.com>
2018-01-28 23:59:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,18 @@ end
 
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+counter = 1
+
 def batch_badge_creator(attendees)
   message_array = attendees.collect { |name| "Hello, my name is #{name}."
 }
   return message_array
 end
+
+def assign_rooms(attendees)
+  attendees.each do |room|
+  puts "Hello #{name}! You'll be assigned to room #{room}!"
+  counter += 1
+end
+end
+  
